c4a9d37cac0901af0998c570d3088fdf1b886c1f
Darya Chekeres <sha@Daryas-MacBook-Air.local>
2019-10-24 23:30:48 +0200

    shape of the rocket

---
+++ rocket.py
@@ -0,0 +1,7 @@
+
+class Rocket:
+    def __init__(self):
+       self.a = Tk.Tk()
+       self.canvas = canvas
+       canvas.create_rectangle(100, 700, 200, 650, fill="grey")
+       self.pack() = canvas.pack()
