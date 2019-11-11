711cc87bbefee454697c609f77d8b31d515f9225
Darya Chekeres <sha@Daryas-MacBook-Air.local>
2019-11-11 17:53:26 +0100

    still not work

--- ex2.py
+++ ex2.py
@@ -2,16 +2,17 @@
 from random import randint
 
 x = randint(30,100)
-print x
-n = 2 
+print x 
 y = raw_input("Vous allez jouer a juste prix, donc vous devez deviner le prix d un objet compris entre 30 et 100 francs. Combien selon vous coute l objet ?")
-if y>x :
-    y = raw_input("c'est moins. Combien?")
-elif y<x :
-    y = raw_input("c'est plus. Combien?")
-else :
-    print "Vous avez gagne!"
-        
+
+while True :
+    if x<y :
+        y = raw_input("c'est moins. Combien?")
+    elif x>y :
+        y = raw_input("c'est plus. Combien?")
+    else :
+        print "Vous avez gagne!"
+        break
 #if  y > x :
  #   print "c'est plus"
   #  y = raw_input("encore?")
