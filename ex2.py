c6832c58dd1cffd05ee853af6fd63afd50de258a
Darya Chekeres <sha@Daryas-MacBook-Air.local>
2019-11-19 21:49:49 +0100

    added : only 10 tentatives; warning at 5; shows the num of tentatives; regame?

--- ex2.py
+++ ex2.py
@@ -1,26 +1,39 @@
 #juste prix
+
 from random import randint
 
-x = randint(30,100)
-print x 
-y = raw_input("Vous allez jouer a juste prix, donc vous devez deviner le prix d un objet compris entre 30 et 100 francs. Combien selon vous coute l objet ?")
-
-while True :
-    if x<y :
-        y = raw_input("c'est moins. Combien?")
-    elif x>y :
-        y = raw_input("c'est plus. Combien?")
-    else :
-        print "Vous avez gagne!"
-        break
-#if  y > x :
- #   print "c'est plus"
-  #  y = raw_input("encore?")
-   # print x
-#if  y < x :
- #   print "c'est moins"
-  #  y = raw_input("encore?")
-   # print x
-#if x == y :
- #   print "Vous avez gagne"
+def justeprix() :
+    x = randint(30,100)
+    print x 
+    y = input("Vous allez jouer a juste prix, donc vous devez deviner le prix d un objet compris entre 30 et 100 francs en 10 tentative. Combien selon vous coute l objet ?")
+    t = 0
+    t = t+1
+    print "tentative numero", t
+    while t<10 :
+        if t == 5 :
+            print "attention, plus que 5 essais"
+        if x<y :
+            y = input("c'est moins. Combien?")
+            t = t+1
+            print "tentative numero", t
+        if x>y :
+            y = input("c'est plus. Combien?")
+            t = t+1
+            print "tentative numero", t
+        if t == 10 :
+            print "vous avez perdu, la reponse etait", x
+            #r = raw_input("voulez vous rejouer?")
+        else :
+            print "Vous avez gagne!"
+            #r = raw_input("voulez vous rejouer?")
+            break
         
+justeprix()
+r = raw_input("voulez vous rejouer?")
+#r = raw_input("voulez vous rejouer?")
+while True :
+    if r == "oui":
+        justeprix()
+        r = raw_input("voulez vous rejouer?")
+    else :
+        break
