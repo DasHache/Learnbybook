4c6b007c3ff698c47a92783f8e0ffeb8454df0c1
kowaraj <kowaraj4stuff@gmail.com>
2020-02-12 14:40:03 +0100

    Learn 'list comprehension' in Python

--- ex3.py
+++ ex3.py
@@ -1,10 +1,9 @@
 #program pendu                                                                  
 J1 = raw_input("joueur 1, donnez le mot a deviner ")
-x = len(J1) + 1
+x = len(J1) 
 print "le mot contient", x,"lettres"
-J2 = raw_input("joueur 2, donnez la lettre a essayer, vous avez le droit a 10 e\
-rreurs ")
-print "la lettre est presente", J1.count(J2), "fois, a la", J1.find(J2)+1, "eme\
- place"
+J2 = raw_input("joueur 2, donnez la lettre a essayer, vous avez le droit a 10 erreurs ")
+print "la lettre est presente", J1.count(J2), "fois, a la", str([i+1 for i in range(len(J1)) if J1[i] == J2]), "eme place"
+
 #print J1.ord(J2)                                                               
 
