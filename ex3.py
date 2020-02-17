777d2c73c20923273eafee173308764253a538bd
Darya Chekeres <sha@Daryas-MacBook-Air.local>
2020-02-17 19:29:50 +0100

    doesn t end the loop

--- ex3.py
+++ ex3.py
@@ -1,19 +1,24 @@
-#program pendu                                                                  
+#programe pendu                                                                  
 J1 = raw_input("joueur 1, donnez le mot a deviner ")
 x = len(J1)
 n = 10
 print "le mot contient", x,"lettres, vous aurez le droit a 10 erreurs"
-while True :
+while n > -1 :
     J2 = raw_input("joueur 2, donnez la lettre a essayer")
     if J1.find(J2) > -1 :    
         print "la lettre est presente", J1.count(J2), "fois, a la",[i+1 for i in range(len(J1)) if J1[i] == J2], "eme place"
+
+# make the found value saved and write it somewhere
     if J1.find(J2) == -1 :
         if n-1> 0 :
             n=n-1
+            print n
             print "vous avez fait une faute, vous avez encore", n, "essaits"
         if n-1< 0 :
             print "vous avez perdu, le mot etait",J1
+            n=n-1
+            print n
             
-            Break    
+        
 #print J1.ord(J2)                                                               
 
