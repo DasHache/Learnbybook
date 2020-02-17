b814607dcccd078f8fc17195506bd16ee9320872
kowaraj <kowaraj4stuff@gmail.com>
2020-02-17 23:34:30 +0100

    Make it look better...

--- ex3.py
+++ ex3.py
@@ -1,14 +1,14 @@
 #programe pendu                                                                  
-J1 = raw_input("joueur 1, donnez le mot a deviner ")
+J1 = raw_input("Joueur 1, donnez le mot a deviner: \n")
 x = len(J1)
 n = 10
-print "le mot contient", x,"lettres, vous aurez le droit a 10 erreurs"
+print "Le mot contient", x,"lettres, vous aurez le droit a 10 erreurs!"
 lettres = [] # 1. Keep all entered letters in this array
 while n > -1 :
-    J2 = raw_input("joueur 2, donnez la lettre a essayer")
+    J2 = raw_input("Joueur 2, donnez la lettre a essayer: \n")
     lettres.append(J2) # 2. Add a new entered letter into the array
     if J1.find(J2) > -1 :    
-        print "la lettre est presente", J1.count(J2), "fois, a la",[i+1 for i in range(len(J1)) if J1[i] == J2], "eme place"
+        print "La lettre est presente", J1.count(J2), "fois, a la", ', '.join([str(i+1) for i in range(len(J1)) if J1[i] == J2]), "eme place"
         # 3. Python operator 'in'
         # 'in' is a Python operator, it means 'element is contained in the array'
         # 1 in [1,2,3] - returns TRUE
@@ -30,7 +30,7 @@ while n > -1 :
             
                        
     if J2 == J1 :
-        print "you won"
+        print "You win!"
         break
 
 # make the found value saved and write it somewhere
@@ -38,9 +38,9 @@ while n > -1 :
         if n-1> -1 :
             n=n-1
         
-            print "vous avez fait une faute, vous avez encore", n, "essaits"
+            print "Vous avez fait une faute, vous avez encore", n, "essaits"
         if n-1< 0 :
-            print "vous avez perdu, le mot etait",J1
+            print "Vous avez perdu, le mot etait",J1
             n=n-1
             
             
