a6c6267816bfa7b21fad772c068da443af93a3b4
Darya Chekeres <sha@Daryas-MacBook-Air.local>
2020-05-04 19:58:45 +0200

    i don't understand several lines in the code

--- Pyqt_Calculator.py
+++ Pyqt_Calculator.py
@@ -1,8 +1,9 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMainWindow, QStatusBar, QToolBar, Qt, QGridLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMainWindow, QStatusBar, QToolBar, QGridLayout
 from PyQt5.QtWidgets import QLineEdit, QPushButton, QVBoxLayout
 
-#app = QApplication([])
+
+# app = QApplication([])
 
 # window = QWidget()
 # window.setWindowTitle('Calculator')
@@ -14,38 +15,57 @@ from PyQt5.QtWidgets import QLineEdit, QPushButton, QVBoxLayout
 class PyCalcul(QMainWindow):
 
     def __init__(self, parent=None):
-
         super().__init__(parent)
         self.setWindowTitle('Calculator')
         self.setGeometry(100, 100, 310, 320)
         self._centralWidget = QWidget(self)
         self.setCentralWidget(self._centralWidget)
-        layout.addWidget(QPushButton('1'), 0, 0)
-        layout.addWidget(QPushButton('2'), 0, 1)
-        layout.addWidget(QPushButton('3'), 0, 2)
-        layout.addWidget(QPushButton('4'), 1, 0)
-        layout.addWidget(QPushButton('5'), 1, 1)
-        layout.addWidget(QPushButton('6'), 1, 2)
-        layout.addWidget(QPushButton('7'), 2, 0)
+        self.generalLayout = QVBoxLayout()
+        self._centralWidget.setLayout(self.generalLayout)
+        self.create_buttons()
+        self.createDisplay()
 
-        # private methods, create different GUI elements
-        # self._createMenu()
-        # self._createToolBar()
-        # self._createStatusBar()
+    def createDisplay(self):
+        self.display = QLineEdit()
+        self.display.setFixedHeight(45)
+        self.display.setAlignment(Qt.AlignRight)
+        self.display.setReadOnly(True)
+        self.generalLayout.addWidget(self.display)
 
-    # def _createMenu(self): # main menu called Menu
-       #  self.menu = self.menuBar().addMenu("&Menu")
-        # self.menu.addAction('&Exit', self.close) # closes the app if pressed
+    def create_buttons(self):
+        self.buttons = {}
+        buttonsLayout = QGridLayout()
+        buttons = {'7': (0, 0),
+                   '8': (0, 1),
+                   '9': (0, 2),
+                   '/': (0, 3),
+                   'C': (0, 4),
+                   '4': (1, 0),
+                   '5': (1, 1),
+                   '6': (1, 2),
+                   '*': (1, 3),
+                   '(': (1, 4),
+                   '1': (2, 0),
+                   '2': (2, 1),
+                   '3': (2, 2),
+                   '-': (2, 3),
+                   ')': (2, 4),
+                   '0': (3, 0),
+                   '00': (3, 1),
+                   '.': (3, 2),
+                   '+': (3, 3),
+                   '=': (3, 4),
+                   }
+        for btnText, pos in buttons.items():
+            self.buttons[btnText] = QPushButton(btnText)
+            self.buttons[btnText].setFixedSize(50, 50)
+            buttonsLayout.addWidget(self.buttons[btnText], pos[0], pos[1])
+            self.generalLayout.addLayout(buttonsLayout)
 
-    # def _createToolBar(self): #  toolbar with a functional Exit tool button
-        # tools = QToolBar()
-        # self.addToolBar(tools)
-        # tools.addAction('Exit', self.close)
 
-   #  def _createStatusBar(self): # status bar at the bottom of the window
-        # status = QStatusBar()
-        # status.showMessage("I'm the Status Bar")
-        # self.setStatusBar(status)
+
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
