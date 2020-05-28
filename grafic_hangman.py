66f93df86d4cc2bf25dd641faa3f33ff36f3e173
Darya Chekeres <sha@Daryas-MacBook-Air.local>
2020-05-29 00:40:16 +0200

    added buttons

--- grafic_hangman.py
+++ grafic_hangman.py
@@ -14,7 +14,7 @@ class View(QMainWindow):
         super().__init__(parent)
 
         self.setWindowTitle('Hangman')
-        self.setGeometry(100, 100, 500, 400)
+        self.setGeometry(100, 100, 800, 700)
 
         self.my_widget = QWidget(self)
         self.setCentralWidget(self.my_widget)
@@ -22,24 +22,66 @@ class View(QMainWindow):
         self.my_layout = QVBoxLayout()
         self.my_widget.setLayout(self.my_layout)
 
-        self.createWordLineEdit()
         self.create_button()
-
+        self.createWordLineEdit()
+        self.create_buttons()
 
     def create_button(self):
-        self.button = QPushButton()
-        self.button.setText("tries")
-        self.button.setFixedSize(200, 300)
-        self.my_layout.addWidget(self.button)
+        self.button = QPushButton("X", self.my_widget)
+        #self.button.setGeometry(300, 300, 100, 100)
+        self.button.move(670, 20)
+        self.button.setFixedSize(100, 100)
+        #self.my_layout.addWidget(self.button)
+
+    def create_buttons(self):
+        self.buttons = {}
+        buttonsLayout = QGridLayout()
+        buttons = {'a': (0, 0),
+                   'b': (0, 1),
+                   'c': (0, 2),
+                   'd': (0, 3),
+                   'e': (0, 4),
+                   'f': (0, 5),
+                   'g': (0, 6),
+                   'h': (0, 7),
+                   'i': (0, 8),
+                   'j': (0, 9),
+                   'k': (0, 10),
+                   'l': (0, 11),
+                   'm': (0, 12),
+                   'n': (1, 0),
+                   'o': (1, 1),
+                   'p': (1, 2),
+                   'q': (1, 3),
+                   'r': (1, 4),
+                   's': (1, 5),
+                   't': (1, 6),
+                   'u': (1, 7),
+                   'v': (1, 8),
+                   'w': (1, 9),
+                   'x': (1, 10),
+                   'y': (1, 11),
+                   'z': (1, 12),
+                   "'": (2, 6),
+                   '-': (2, 7),
+                   }
+        for btnText, pos in buttons.items():
+            self.buttons[btnText] = QPushButton(btnText)
+            self.buttons[btnText].setFixedSize(60, 60)
+            buttonsLayout.addWidget(self.buttons[btnText], pos[0], pos[1])
+        self.my_layout.addLayout(buttonsLayout)
 
     def createWordLineEdit(self):
-        self.word_LineEdit = QLineEdit()
-        self.word_LineEdit.setFixedHeight(45)
+        self.word_LineEdit = QLineEdit("", self.my_widget)
+        #self.word_LineEdit.setFixedHeight(300)
+        #self.word_LineEdit.setFixedWidth(600)
+        self.word_LineEdit.setFixedSize(600, 40)
+        self.word_LineEdit.move(20, 20)
         self.word_LineEdit.setFont(QFont('NewTimesRoman', 30)) 
         self.word_LineEdit.setAlignment(Qt.AlignLeft)
-        self.word_LineEdit.setPlaceholderText("Enter a word and press 'Enter'...")
+        self.word_LineEdit.setPlaceholderText("Enter a word to guess and press 'Enter'...")
         self.word_LineEdit.setReadOnly(False)
-        self.my_layout.addWidget(self.word_LineEdit)
+        #self.my_layout.addWidget(self.word_LineEdit)
 
 
 class Controler:
